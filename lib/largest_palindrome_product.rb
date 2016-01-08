80f3b24394299545068b8a4aad7c1ae095453bfe
hdwatts <howard@hdwatts.com>
2016-01-08 00:21:33 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,30 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  num1 = 999
+  num2 = 999
+  palindrome = -1
+  largest = -1
+  min = 900 #performance saving number, no need to go lower since we just want the largest palindrome
+
+  while num1 > min
+    while num2 > min
+      palindrome = is_palindrome?(num1 * num2)
+      largest = palindrome if palindrome > largest
+      num2 -= 1
+    end
+    num2 = 999
+    num1 -= 1
+  end
+
+  largest
+end
+
+def is_palindrome?(input)
+  forwards = "#{input}"
+  backwards = forwards.reverse
+
+  if backwards == forwards
+    input
+  else
+    -1
+  end
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,36 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def initialize()
+  end
+
+  def answer
+    num1 = 999
+    num2 = 999
+    palindrome = -1
+    largest = -1
+    min = 900 #performance saving number, no need to go lower since we just want the largest palindrome
+
+    while num1 > min
+      while num2 > min
+        palindrome = is_palindrome?(num1 * num2)
+        largest = palindrome if palindrome > largest
+        num2 -= 1
+      end
+      num2 = 999
+      num1 -= 1
+    end
+
+    largest
+  end
+
+  def is_palindrome?(input)
+    forwards = "#{input}"
+    backwards = forwards.reverse
+
+    if backwards == forwards
+      input
+    else
+      -1
+    end
+  end
+end
